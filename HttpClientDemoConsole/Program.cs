b0c9efa748e1f4f7c2d40a34e30eabc3f4de0f45
Cruz Sanchez <cruzsanchez9209@gmail.com>
2020-07-18 09:34:10 -0500

    Add project files.

---
+++ HttpClientDemoConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace HttpClientDemoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var users = GetJsonData();
+
+            foreach (var user in users)
+            {
+                Console.WriteLine($"{user.id} : {user.title}");
+            }
+        }
+
+        public static IEnumerable<User> GetJsonData()
+        {
+            List<User> users = new List<User>();
+            var client = new HttpClient();
+            var response = client.GetStringAsync(new Uri("https://jsonplaceholder.typicode.com/posts")).Result;
+
+            var jArray = JArray.Parse(response);
+
+            foreach (var token in jArray)
+            {
+                users.Add(JsonConvert.DeserializeObject<User>(token.ToString()));
+            }
+
+            return users;
+        }
+    }
+}
